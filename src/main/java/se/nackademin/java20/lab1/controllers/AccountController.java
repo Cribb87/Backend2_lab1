359b27f3db171f611c3462ab78cdcf3e394dfb90
simonnojd <simon.nojd.jobb@gmail.com>
2021-08-25 10:34:21 +0200

    Made controllers for Users and Accounts

--- src/main/java/se/nackademin/java20/lab1/controllers/AccountController.java
+++ src/main/java/se/nackademin/java20/lab1/controllers/AccountController.java
@@ -1,11 +1,19 @@
 package se.nackademin.java20.lab1.controllers;
 
-/**
- * Created by Christoffer Grännby
- * Date: 2021-08-25
- * Time: 10:15
- * Project: Övningsuppgifter
- * Copyright: MIT
- */
+import org.springframework.web.bind.annotation.*;
+import se.nackademin.java20.lab1.models.Account;
+import se.nackademin.java20.lab1.services.AccountService;
+
+@RestController
+@RequestMapping(path = "/account")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping(path = "/create")
+    public @ResponseBody String createAccount (@ResponseBody Account account) {
+        accountService.saveAccount(account);
+        return "Account created";
+    }
+
 }
