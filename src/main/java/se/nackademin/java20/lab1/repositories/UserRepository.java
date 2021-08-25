4e400088949b7340b6d38554858780cc28e335b0
Christoffer Grännby <cribbster1987@hotmail.com>
2021-08-25 10:29:58 +0200

    repositories now extends JpaRepository

--- src/main/java/se/nackademin/java20/lab1/repositories/UserRepository.java
+++ src/main/java/se/nackademin/java20/lab1/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package se.nackademin.java20.lab1.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.nackademin.java20.lab1.models.User;
+
 /**
  * Created by Christoffer Grännby
  * Date: 2021-08-25
@@ -7,5 +11,8 @@ package se.nackademin.java20.lab1.repositories;
  * Project: Övningsuppgifter
  * Copyright: MIT
  */
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
 }
